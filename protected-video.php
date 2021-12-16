202bbe5ebaa215cd8be8cdf09e774021f46fb67e
Alec Rust <me@alecrust.com>
2021-12-16 20:54:46 +0000

    Upgrade Plyr version 3.6.11 -> 3.6.12

--- protected-video.php
+++ protected-video.php
@@ -28,7 +28,7 @@ define('PROTECTED_VIDEO_VERSION', '1.5.0');
 /**
  * Plyr player version.
  */
-define('PLYR_VERSION', '3.6.11');
+define('PLYR_VERSION', '3.6.12');
 
 /**
  * Load core plugin class defining all hooks.
