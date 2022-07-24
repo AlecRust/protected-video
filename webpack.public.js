82456a9fb52ab61f42e8ace44ac7f39652e0b3b8
Alec Rust <me@alecrust.com>
2022-07-24 11:28:53 +0100

    Switch to official WordPress Babel preset

--- webpack.public.js
+++ webpack.public.js
@@ -20,7 +20,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env'],
+            presets: ['@wordpress/babel-preset-default'],
           },
         },
       },
