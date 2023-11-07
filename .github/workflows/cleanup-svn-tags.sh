fcba3f8e0267ea54721c9184d45c041d4792beb2
Alec Rust <me@alecrust.com>
2023-11-07 17:02:31 +0000

    Automate deletion of old plugin SVN tags
    Ref:
    https://github.com/10up/action-wordpress-plugin-deploy/issues/70
    https://developer.wordpress.org/plugins/wordpress-org/plugin-developer-faq/#how-many-old-releases-should-i-keep-in-svn

---
+++ .github/workflows/cleanup-svn-tags.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Set SVN repo URL
+SVN_REPO_URL="https://plugins.svn.wordpress.org/protected-video/"
+
+# Fetch all tags from SVN
+svn_tags=$(svn ls "$SVN_REPO_URL/tags" --username "$SVN_USERNAME" --password "$SVN_PASSWORD" --non-interactive --no-auth-cache)
+
+# Parse tags, sort them, and get all but the latest 10
+old_tags=$(echo "$svn_tags" | sort -V | head -n -10)
+
+# Loop through old tags and delete them
+for tag in $old_tags; do
+    svn delete "$SVN_REPO_URL/tags/$tag" --username "$SVN_USERNAME" --password "$SVN_PASSWORD" -m "Deleting old tag: $tag" --non-interactive --no-auth-cache
+done
