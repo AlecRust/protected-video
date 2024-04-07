9839b5de311e6426a9daf070abca564257d22493
Alec Rust <me@alecrust.com>
2024-04-07 17:00:39 +0100

    Improve variable names

--- .cliff/build-changelogs.js
+++ .cliff/build-changelogs.js
@@ -2,12 +2,12 @@ const { execSync } = require('child_process')
 const packageJson = require('../package.json')
 
 const nextVersion = packageJson.version
-const cliffChangelogCmd = `git-cliff --config .cliff/cliff-changelog-md.toml -o CHANGELOG.md --tag v${nextVersion}`
-const cliffReadmeCmd = `git-cliff --config .cliff/cliff-readme-txt.toml -o readme.txt --tag v${nextVersion}`
+const changelogMdCmd = `git-cliff --config .cliff/cliff-changelog-md.toml -o CHANGELOG.md --tag v${nextVersion}`
+const readmeTxtCmd = `git-cliff --config .cliff/cliff-readme-txt.toml -o readme.txt --tag v${nextVersion}`
 
 try {
-  execSync(cliffChangelogCmd, { stdio: 'inherit' })
-  execSync(cliffReadmeCmd, { stdio: 'inherit' })
+  execSync(changelogMdCmd, { stdio: 'inherit' })
+  execSync(readmeTxtCmd, { stdio: 'inherit' })
 } catch (error) {
   console.error('Failed to build changelogs:', error)
   process.exit(1)
