51a996b8ab41d237f00d58a0b7a4f41900d54499
Alec Rust <me@alecrust.com>
2024-04-11 13:10:35 +0100

    Improve tag deletion SVN commit message

--- .github/workflows/cleanup-svn-tags.sh
+++ .github/workflows/cleanup-svn-tags.sh
@@ -10,5 +10,5 @@ old_tags=$(echo "$svn_tags" | sort -V | head -n -10)
 
 # Loop through old tags and delete them
 for tag in $old_tags; do
-    svn delete "$SVN_REPO_URL/tags/$tag" --username "$SVN_USERNAME" --password "$SVN_PASSWORD" -m "Deleting old tag: $tag" --non-interactive --no-auth-cache
+    svn delete "$SVN_REPO_URL/tags/$tag" --username "$SVN_USERNAME" --password "$SVN_PASSWORD" -m "Delete old tag: $tag" --non-interactive --no-auth-cache
 done
