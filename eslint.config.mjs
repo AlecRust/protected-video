6ca926c5e2f51f1cc92066850623954ad525910a
Alec Rust <me@alecrust.com>
2025-12-21 11:31:47 +0000

    Upgrade ESLint v8 -> v9

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import globals from 'globals';
+
+export default [
+	{
+		ignores: ['build/**', 'demo/**', 'vendor/**', 'node_modules/**'],
+	},
+	js.configs.recommended,
+	eslintConfigPrettier,
+	{
+		languageOptions: {
+			ecmaVersion: 'latest',
+			sourceType: 'module',
+			parserOptions: {
+				ecmaFeatures: {
+					jsx: true,
+				},
+			},
+			globals: {
+				...globals.browser,
+				...globals.node,
+				Plyr: 'readonly',
+			},
+		},
+	},
+];
