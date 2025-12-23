52221ecacdfb1e7026ba7a2bd109d3420434ae8c
Alec Rust <me@alecrust.com>
2025-12-23 14:41:45 +0000

    Align 7.2 as the minimum PHP version

--- protected-video.php
+++ protected-video.php
@@ -7,6 +7,7 @@
  * Version:           1.11.6
  * Author:            Alec Rust
  * Author URI:        https://www.alecrust.com/
+ * Requires PHP:      7.2
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       protected-video
